73662ae8518760638aca05f2c040a9cf14713f6a
Jonathan Gerard <jonathan.gerard@hotmail.se>
2022-04-25 19:12:46 +0200

    starting feed components

--- jonathan-prototyp/vue-project/src/App.vue
+++ jonathan-prototyp/vue-project/src/App.vue
@@ -1,10 +1,16 @@
-<script setup>
-
-</script>
-
 <template>
 
 </template>
+
+<script>
+export default{
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
 
 <style>
 </style>
---
+++ jonathan-prototyp/vue-project/src/components/Review.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="alias">
+        Per
+    </div>
+    <div class="rating">
+        5
+    </div>
+    <div class="text-review">
+        Bra grepp. Skön mjuk yta. Skonsam mot underlag
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
